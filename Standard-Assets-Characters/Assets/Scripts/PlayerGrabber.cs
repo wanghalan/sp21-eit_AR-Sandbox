6f3bec52776643555c5674f40c477b0e5d5d435c
Your Name <liuxun1230@hotmail.com>
2021-04-10 00:53:21 -0400

    kinect real time mesh

---
+++ Standard-Assets-Characters/Assets/Scripts/PlayerGrabber.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGrabber : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (player!= null)
+        {
+            player.transform.position = this.transform.position;
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
